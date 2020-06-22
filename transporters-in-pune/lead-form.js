a5a478ba2df906c5df7e4844f8732a42baae6e10
Saurabh Katkar <saurabh.katkar102@gmail.com>
2020-06-22 18:50:11 +0530

    meta tag changes

--- transporters-in-pune/lead-form.js
+++ transporters-in-pune/lead-form.js
@@ -7,4 +7,45 @@ $(document).ready(function () {
       $("select.vehicleType").hide();
     }
   });
+
+  $("#place").autocomplete({
+    source: placeAC,
+    minLength: 2,
+    select: function (event, ui) {
+      console.log(
+        "Selected: " + ui.item.value + " with LocationId " + ui.item.id
+      );
+    },
+  });
+
+  function placeAC(query, callback) {
+    $.getJSON(
+      "https://places.cit.api.here.com/places/v1/autosuggest?at=" +
+        coordinates +
+        "&q=" +
+        query.term +
+        "&app_id=" +
+        APP_ID_HERE +
+        "&app_code=" +
+        APP_CODE_HERE,
+      function (data) {
+        var places = data.results.filter((place) => place.vicinity);
+
+        places = places.map((place) => {
+          return {
+            title: place.title,
+            value:
+              place.title +
+              ", " +
+              place.vicinity.replace(/<br\/>/g, ", ") +
+              " (" +
+              place.category +
+              ")",
+            id: place.id,
+          };
+        });
+        return callback(places);
+      }
+    );
+  }
 });
